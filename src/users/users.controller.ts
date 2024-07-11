a8ab0c8e20a3dc0bd8693c18b9c5c5850b38afe4
do-not-do-that <rl7968@naver.com>
2024-07-11 12:26:46 +0900

    feat : custom interceptor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,10 +8,13 @@ import {
   Patch,
   Post,
   Query,
+  UseInterceptors,
+  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
 
 @Controller('auth')
 export class UsersController {
@@ -21,8 +24,10 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
+  @UseInterceptors(SerializeInterceptor)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
+    console.log('handler is running');
     const user = await this.usersService.findOne(parseInt(id));
     if (!user) {
       throw new NotFoundException(`User ${id} not found`);
