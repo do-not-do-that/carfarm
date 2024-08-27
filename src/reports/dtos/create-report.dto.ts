adcab5d7005a18fe57b1c8d64da14acd78b69efc
do-not-do-that <rl7968@naver.com>
2024-08-27 20:39:36 +0900

    feat : add report create feature

---
+++ src/reports/dtos/create-report.dto.ts
@@ -0,0 +1,37 @@
+import {
+  IsLatitude,
+  IsLongitude,
+  IsNumber,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+
+export class CreateReportDto {
+  @IsString()
+  make: string;
+
+  @IsString()
+  model: string;
+
+  @IsNumber()
+  @Max(2050)
+  @Min(1930)
+  year: number;
+
+  @IsNumber()
+  @Max(1000000)
+  @Min(0)
+  mileage: number;
+
+  @IsLongitude()
+  lng: number;
+
+  @IsLatitude()
+  lat: number;
+
+  @IsNumber()
+  @Max(1000000)
+  @Min(0)
+  price: number;
+}
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Report } from './report.entity';
+import { Repository } from 'typeorm';
+import { CreateReportDto } from './dtos/create-report.dto';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+  async create(creportDto: CreateReportDto) {
+    const report = this.repo.create(creportDto);
+    return this.repo.save(report);
+  }
+}
